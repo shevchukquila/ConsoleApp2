0cc4d5b70cc3a431591a3205174b26f759052b86
shevchukquila <102552534+shevchukquila@users.noreply.github.com>
2023-05-27 13:40:58 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[8];
+            int[] array = { 37, 0, 50, 46, 34, 46, 0, 13 };
+            foreach (int element in array)
+            {
+                Console.Write($"{element}");
+            }
+            Console.Read();
+        }
+    }
+}
